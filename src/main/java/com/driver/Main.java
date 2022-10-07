cbcc48cdb7b36460c9395415fe7bfac23dc5eb52
Tushar <tushargarg0110@gmail.com>
2022-10-07 21:22:25 +0530

    Encapsulation code working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,13 +1,12 @@
 package com.driver;
 
 public class Main {
-
     public static void main(String[] args){
 
         RWOnly obj = new RWOnly();
-
+        //obj.name = "Tushar";
+        //java: name has private access in com.driver.RWOnly
         obj.setName("Tushar");
-
         System.out.println(obj.getName());
     }
   
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class RWOnly {
 
-   private String name;
+    private String name;
 
     public String getName() {
         return name;
