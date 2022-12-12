a2e4114457ba13f8884f79dbd84846f4afc7bb1e
Tushar <tushargarg0110@gmail.com>
2022-12-12 11:41:15 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,13 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
 
-        RWOnly obj = new RWOnly();
+        RWOnly obj  = new RWOnly();
+
+        //'name' has private access in 'com.driver.RWOnly'
         //obj.name = "Tushar";
-        //java: name has private access in com.driver.RWOnly
-        obj.setName("Tushar");
+
+        obj.setName("Rauch");
+
         System.out.println(obj.getName());
     }
   
